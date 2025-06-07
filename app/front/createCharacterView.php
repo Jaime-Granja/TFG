1fea29d06dbeaa18be16bd88e693085c0d6a49ee
Alejandro Valencia Marin <163522939+Alejandro-Valencia-Marin@users.noreply.github.com>
2025-06-07 10:36:05 +0200

    Añadido nombre al archivo para que no sea document

--- app/front/createCharacterView.php
+++ app/front/createCharacterView.php
@@ -69,7 +69,7 @@ if (isset($_POST['createCharacter'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../src/styles/stylesCreateCharacter.css" />
     <link rel="shortcut icon" href="../src/img/logo.png" />
-    <title>Document</title>
+    <title>BDD-Character</title>
 </head>
 
 <body>
