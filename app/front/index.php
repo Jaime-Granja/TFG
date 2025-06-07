f58b5cb9884f3b963b9cd98c5e4bd09faf903d08
Alejandro Valencia Marin <163522939+Alejandro-Valencia-Marin@users.noreply.github.com>
2025-06-07 10:37:06 +0200

    Añadido nombre al archivo para que no sea document

--- app/front/index.php
+++ app/front/index.php
@@ -90,7 +90,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>BDD-index</title>
     <link href="https://fonts.googleapis.com/css2?family=Almendra&family=Cormorant+Garamond:wght@700&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="../src/styles/styles.css" />
